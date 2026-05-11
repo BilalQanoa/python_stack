61ef4189834db5a84b8195c361c83f01e0276fb6
BilalQanoa <bilalqanoa@gmail.com>
2026-05-11 10:00:17 +0300

    edit counter

--- python_stack/Flask/counter/server.py
+++ python_stack/Flask/counter/server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 
-app.secret_key = 'your_super_secret_key_here' 
+app.secret_key = 'super_secret_key_for_counter_app'
 
 @app.route('/')
 def index():
@@ -13,21 +13,21 @@ def index():
         session['visits'] = 0
 
     if 'count' not in session:
-        session['count'] = 1
-    
+        session['count'] = 0
         
     return render_template("index.html")
 
-@app.route('/add_two')
-def add_two():
+@app.route('/add_count2')
+def add_count2():
 
-    session['count'] += 1
+    session['count'] += 2
     session['visits'] -= 1
     return redirect('/')
 
 @app.route('/reset')
 def reset_count():
-    session['count'] = 0 
+    session['count'] = 0
+    session['visits'] -= 1 
     return redirect('/')
 
 @app.route('/custom_increment', methods=['POST'])
@@ -36,15 +36,14 @@ def custom_increment():
     inc_value = request.form.get('increment')
     if inc_value and inc_value.isdigit():
 
-        session['count'] += int(inc_value) - 1
+        session['count'] += int(inc_value)
         session['visits'] -= 1
     return redirect('/')
 
 @app.route('/destroy_session')
 def destroy():
     if 'visits' in session:
-        session.pop('visits')
-    # session.clear()
+        session.clear()
     return redirect('/')
 
 if __name__ == "__main__":
